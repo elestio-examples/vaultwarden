2c871efc85995ab275b3e6cee0687f752724168c
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-08-13 12:08:10 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,9 +2,3 @@
 set -o allexport; source .env; set +o allexport;
 
 
-TOKEN=$(echo -n "${ADMIN_TOKEN}" | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4)
-
-cat << EOT > ./.env
-
-ADMIN_PASSWORD=${TOKEN}
-EOT
